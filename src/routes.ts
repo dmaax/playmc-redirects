5e0904fd72ac33f62974ecd3e2ea89b8c3ec37d8
dmaax <dudzcool@gmail.com>
2023-04-12 04:41:46 -0300

    Add live redirect

--- src/routes.ts
+++ src/routes.ts
@@ -6,9 +6,14 @@ const shopRedirect = ctx => {
     return ctx.redirect('https://loja.playnetwork.com.br', 301);
 };
 
+const liveRedirect = ctx => {
+    return ctx.redirect('https://www.youtube.com/c/EduPlaysmdhapc/live', 301);
+};
+
 export default app => {
     app.get('/d', discordRedirect)
     app.get('/discord', discordRedirect)
     app.get('/l', shopRedirect)
     app.get('/loja', shopRedirect)
+    app.get('/live', liveRedirect)
 }
